a35eb43f7027d7e9ccea672188ff2197ed764a62
join010255 <mohammedlelly2006@gmail.com>
2026-09-06 16:28:24 -0700

    update dll injection

--- dll_injection.py
+++ dll_injection.py
@@ -1,18 +1,38 @@
+import os
+import sys
+import psutil
 import win32process
 import win32con
 import win32api
 import win32event
+from colorama import Fore, Style, init
+
+init()
+
 
 class DllInjection:
-    def __init__(self) -> None:
-        self.dll_path = r"C:\Users\yoi\Desktop\dll_injection\MsgBoxDLL.dll"
-        self.inject()
+    def __init__(self, process_name=None, process_id=None, dll_path: str) -> None:
+        if process_name:
+            pid: int = self.get_pid(process_name)
+            if pid:
+                self.inject(pid, dll_path)
+        else:
+            self.inject(process_id, dll_path)
+
+
+    def get_pid(process_name: str) -> int:
+        for process in psutil.process_iter(["name", "pid"]):
+            if process.info["name"].lower() == process:
+                parent: bool = process.parent()
+                if parent:
+                    return parent.pid()
     
-    def inject(self) -> None:
+    def inject(self, process_id: int, dll_path: str) -> None:
         process_acess = win32con.PROCESS_ALL_ACCESS
         hProcess = win32api.OpenProcess(process_acess, False, 1612)
         if not hProcess:
             raise Exception("[-] Failed to create process")
+            
         print(f"[+] Process created with PID: {1612}")
         dll_path_bytes: bytes = self.dll_path.encode("mbcs")+b'/x00'
 
@@ -25,19 +45,17 @@ class DllInjection:
             win32con.MEM_COMMIT | win32con.MEM_RESERVE,
             win32con.PAGE_READWRITE
         )
-        print(hex(address))
         if not address:
             raise Exception("[-] Failed to allocate memory")
-        writeProcess = win32process.WriteProcessMemory(hProcess, address, dll_path_bytes)
-        print(f"[+] dll injection : {writeProcess}")
+        writeProcess_memory: int = win32process.WriteProcessMemory(hProcess, address, dll_path_bytes)
 
+        if not writeProcess_memory:
+            raise Exception("[-] Failed to allocate memory")
         
         handel = win32api.GetModuleHandle("kernel32.dll")
         loadlibrary_address = win32api.GetProcAddress(handel, b"LoadLibraryA")
         if not loadlibrary_address:
             return Exception("[-] loade is not true")
-
-        print(hex(loadlibrary_address))
 
         hThread = win32process.CreateRemoteThread(
             hProcess,
@@ -47,12 +65,37 @@ class DllInjection:
             address,
             0
         )
-        print(hThread)
+        if not hThread:
+            print("lala")
+
+            
         result = win32event.WaitForSingleObject(hThread[0], 5000)  # 5 second timeout
         win32api.CloseHandle(hThread[0])
         win32api.CloseHandle(hProcess)
 if __name__ == "__main__":
     try:
-        DllInjection()
+        print(Fore.RED+r"""
+            ________  .__  .__    .___            __               __                
+            \______ \ |  | |  |   |   | ____     |__| ____   _____/  |_  ___________ 
+             |    |  \|  | |  |   |   |/    \    |  |/ __ \_/ ___\   __\/  _ \_  __ \
+             |    `   \  |_|  |__ |   |   |  \   |  \  ___/\  \___|  | (  <_> )  | \/
+            /_______  /____/____/ |___|___|  /\__|  |\___  >\___  >__|  \____/|__|   
+                    \/                     \/\______|    \/     \/                   
+        """+Style.NORMAL)
+        print("-"*121)
+        dll_path: str = input(Fore.YELLOW+"[+] Enter Dll Path: "+Style.NORMAL).strip()
+        if not os.path.exists(dll_path):
+            print(Fore.RED+"[-] Dll Path Is Not Exists"+Style.NORMAL)
+            sys.exit(0)
+            
+        process_id: str = input(Fore.YELLOW+"[+]  Process Id (scape): "+Style.NORMAL).strip()
+        if not process_id:
+            process_name: str = input(Fore.YELLOW+"[+] Process Name : "+Style.NORMAL)
+            DllInjection(process_name=process_name, dll_path=dll_path)
+            
+        else:
+            DllInjection(process_id=process_id, dll_path=dll_path)
+        
+        
     except Exception as e:
         print(f"[-] Error: {e}")
